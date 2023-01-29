9064fa52ab78a6c96b4b94cf19077ba8d05e13e4
Fhwang <donghwa.dev@gmail.com>
2023-01-29 19:28:25 +0900

    Update cert.sh

--- app/cert.sh
+++ app/cert.sh
@@ -4,5 +4,3 @@ sudo openssl s_client -showcerts -connect harbor.donghwa.dev:4443 < /dev/null | 
 sudo cat dndev.crt
 sudo cp dndev.crt /usr/local/share/ca-certificates/
 sudo update-ca-certificates
-sudo systemctl restart docker
-sudo docker info
